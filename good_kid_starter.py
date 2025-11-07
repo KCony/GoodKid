2ad4d82a9795e6d863087805eb0a0c9fa6859822
Konstantin Kuzmin <kmkuzmin@gmail.com>
2025-11-07 11:13:26 -0500

    Added all Python files from an existing project

---
+++ good_kid_starter.py
@@ -0,0 +1,9 @@
+
+testcase_cnt = int(input())
+for _ in range(testcase_cnt):
+    _ = input()
+    summa = []
+    arr = [int(elem) for elem in input().split()]
+    res = 0
+    # Implement Good kid algorithm
+    print(res)
